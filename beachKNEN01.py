9f73f63bb00f506bdcd6d32496253fdf317c3f9e
Kaylee Nieves <kayleenieves@Kaylees-MacBook-Pro.local>
2019-03-26 12:16:02 -0400

    Water added

--- beachKNEN01.py
+++ beachKNEN01.py
@@ -8,3 +8,8 @@ beachWin.setCoords(0,0,500,500)
 sRec = Rectangle(Point(0, 0), Point(500, 100))
 sRec.setFill("orange")
 sRec.draw(beachWin) 
+
+#Water
+wRec = Rectangle(Point(0, 100), Point(500, 300))
+wRec.setFill("dark blue")
+wRec.draw(beachWin) 
