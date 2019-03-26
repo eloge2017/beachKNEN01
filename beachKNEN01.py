62d8db5bdf220b2dc8c1fef72c5f2838051d4985
Kaylee Nieves <kayleenieves@Kaylees-MacBook-Pro.local>
2019-03-26 11:55:05 -0400

    Sand added

--- beachKNEN01.py
+++ beachKNEN01.py
@@ -3,3 +3,8 @@ from graphics import*
 
 beachWin = GraphWin("Beach!" , 900, 500)
 beachWin.setCoords(0,0,500,500)
+
+#Sand
+sRec = Rectangle(Point(0, 0), Point(500, 100))
+sRec.setFill("orange")
+sRec.draw(beachWin) 
