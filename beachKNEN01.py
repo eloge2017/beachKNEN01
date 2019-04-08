e3b9f290b3943a6c80d9bfd9b476ff176433221a
eloge ndja <eloge.ndja@uamaker.nyc>
2019-04-08 10:42:44 -0400

    created functions for the rectangles

--- beachKNEN01.py
+++ beachKNEN01.py
@@ -2,42 +2,35 @@
 from graphics import*
 
 beachWin = GraphWin("Beach!" , 900, 500)
-beachWin.setCoords(0,0,500,500)
+beachWin.setCoords(0,0,900,500)
 
-
-
-#Sky
-skyRec = Rectangle(Point(0, 250), Point(500, 900))
-skyRec.setFill("light blue")
-skyRec.draw(beachWin)
-
-#Sun
-sunCir = Oval(Point(150, 400), Point(350, 100))
-sunCir.setFill("yellow")
-sunCir.draw(beachWin)
-
-Birds  
-birdImage = Image(Point(250,370), "Unknown.png")
-birdImage.draw(beachWin)
-
-#Sand
-sRec = Rectangle(Point(0, 0), Point(500, 100))
-sRec.setFill("orange")
-sRec.draw(beachWin) 
-
-#Water
-wRec = Rectangle(Point(0, 100), Point(500, 250))
-wRec.setFill("dark blue")
-wRec.draw(beachWin)
+def draw_Rec(rX, rY, size, color, win):
+     rectangle = Rectangle(rX,rY)
+     rectangle.setFill(color)
+     rectangle.draw(win)
 
 
 def draw_Cir(cX, cY, size, color, Win):
-     for i in range (6): 
-        circle = Circle(Point(cX, cY),(75))
+     for i in range (10): 
+        circle = Circle(Point(cX, cY),(85))
         circle.setFill(color)
         circle.setOutline(color)
         circle.draw(Win)
-   
+        
+#Sky
+draw_Rec(Point(0, 250), Point(900, 500), 50,"light blue", beachWin)
+     
+#Sun
+sunCir = Oval(Point(150, 430), Point(750, 90))
+sunCir.setFill("yellow")
+sunCir.draw(beachWin)
+
+#Sand
+draw_Rec(Point(0, 0), Point(900, 100), 50, "orange", beachWin)
+ 
+#Water
+draw_Rec(Point(0, 100), Point(900, 250), 50, "dark blue", beachWin)
+
 size=70
-for z in range(6):
+for z in range(10):
     draw_Cir(z * 100,185 , size, "dark blue", beachWin)
