366611d78dd4879177c79bccca5ca075a3cf895b
Kaylee Nieves <kayleenieves@Kaylees-MacBook-Pro.local>
2019-04-10 12:43:03 -0400

    fishes are a function now

--- beachKNEN01.py
+++ beachKNEN01.py
@@ -4,6 +4,26 @@ from graphics import*
 beachWin = GraphWin("Beach!" , 900, 500)
 beachWin.setCoords(0,0,900,500)
 
+def draw_Fish(xPos,yPos,fishCol1,fishCol3,fishCol2,Win):
+    #x = 695
+    #y = 150
+    
+    #fish tail
+    fishTail = Polygon(Point(xPos+5,yPos-20), Point(xPos+65,yPos), Point(xPos+65,yPos-40))
+    fishTail.setFill(fishCol1)
+    fishTail.draw(Win)
+
+    #fish body
+    fishBody = Oval(Point(xPos,yPos), Point(xPos+50,yPos-40))
+    fishBody.setFill(fishCol3)
+    fishBody.draw(Win)
+
+    #fish eye
+    fishEye = Circle(Point(xPos+5,yPos-15), 4)
+    fishEye.setFill(fishCol2)
+    fishEye.draw(Win)
+
+
 #Sky
 skyRec = Rectangle(Point(0, 250), Point(900, 500))
 skyRec.setFill("light blue")
@@ -24,23 +44,7 @@ wRec = Rectangle(Point(0, 100), Point(900, 250))
 wRec.setFill("dark blue")
 wRec.draw(beachWin)
 
-#Fish One
-#fish tail
-fishTail = Polygon(Point(700,130), Point(760,150), Point(760,110))
-fishTail.setFill("magenta")
-fishTail.draw(beachWin)
-
-#fish body
-fishBody = Oval(Point(695,150), Point(745,110))
-fishBody.setFill(color_rgb(0,175,200))
-fishBody.draw(beachWin)
-
-#fish eye
-fishEye = Circle(Point(700,135), 4)
-fishEye.setFill("black")
-fishEye.draw(beachWin)
-
-
-
-
-
+draw_Fish(700,150,"magenta","red","black",beachWin)
+draw_Fish(100,200,"yellow","pink","black",beachWin)
+draw_Fish(400,175,"blue","orange","black",beachWin)
+    
